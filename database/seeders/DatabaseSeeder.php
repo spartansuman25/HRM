cf41e7e108babd863b4c654012d8e07828fe60a1
Spartan <spartansuman25@gmail.com>
2022-12-07 14:30:47 +0545

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         User::factory(10)->create();
+
+         User::factory()->create([
+             'name' => 'Techmauri',
+             'email' => 'techmauri.com',
+         ]);
+    }
+}
